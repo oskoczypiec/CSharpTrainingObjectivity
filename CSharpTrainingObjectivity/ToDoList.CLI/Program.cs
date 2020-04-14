fbc5847984fa7aa306dddb913c252a84366b0c36
Aleksandra Skoczypiec <askoczypiec@objectivity.co.uk>
2020-04-14 17:39:10 +0200

    Training day 2

---
+++ CSharpTrainingObjectivity/ToDoList.CLI/Program.cs
@@ -0,0 +1,39 @@
+﻿using Objectivity.CSharp.Interfaces;
+using Objectivity.CSharp.Interfaces.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.Logic;
+
+namespace ToDoList.CLI
+{
+    class Program
+    {
+        static ToDoListEngineInterface engine;
+        static void Main(string[] args)
+        {
+            engine = new ToDoListEngine();
+            Console.WriteLine("To jest moja todo lista");
+            InitTestData(3);
+            Console.ReadKey();
+        }
+
+        static void InitTestData(int numberOfElements)
+        {
+            //petle
+            Random rand = new Random();
+
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                engine.AddTask(new ToDoTask()
+                {
+                    Id = i,
+                    Title = $"Title{rand.Next(100)}",
+                    Description = $"Description{rand.Next(100)}"
+                });
+            }
+        }
+    }
+}
---
+++ CSharpTrainingObjectivity/ToDoList.Logic/ToDoListEngine.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Objectivity.CSharp.Interfaces;
+using Objectivity.CSharp.Interfaces.Properties;
+
+namespace ToDoList.Logic
+{
+    public class ToDoListEngine : ToDoListEngineInterface
+    {
+        List<ToDoTask> taskList;
+        public ToDoListEngine()
+        {
+            taskList = new List<ToDoTask>();
+        }
+        public void AddTask(ToDoTask task)
+        {
+            taskList.Add(task);
+        }
+
+        public List<ToDoTask> FindTask(string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ToDoTask> FindTask(string query, Objectivity.CSharp.Interfaces.TaskStatus status, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ToDoTask> GetAllTasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ToDoTask> GetTaskDueToday()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ToDoTask> GetTasks(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ToDoTask> GetTasks(DateTime start, DateTime end)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateTask(ToDoTask task)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
